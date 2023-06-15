25c941fc7d14432d69131a6e5fab6a3e852d6e6b
suryabhan29 <suryabhansingh1029@gmail.com>
2023-06-15 17:01:27 +0530

    javascript function topic

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// There are two things related with function
+
+// code always check Top to Bottom 
+
+// 2.  Function calling/invoking  call/invok
+
+// Function define  and calling both are doing same time 
+
+getmyname(); // this is function calling 
+
+getMySurname();  // this is function calling
+
+// 1. Function Defination (one time process)
+
+function getmyname(){     // this is not follow camalCase
+    // Function body
+    console.log('SURYABHAN');  // statement/instructions
+}
+
+function getMySurname(){   // this is follow camalCase 
+    // Function body
+    // Inside the function body i write many many statement (;)
+    // Every line close the semi colon call statement
+    console.log("SISOIDYA");  // Statement/Instructions
+}
+
+// Function name always write camalCase
+//2. function call
+getMyAddress('Neemuch Mp India');  // actural argument
+
+//1. function define
+function getMyAddress(adrr){  // formal argument
+    //function body
+    console.log(adrr)
+} 
+            // Define time = formal arrgumente
+
+            // calling time = actural arrgument
+
+            // = `` '' "" (this all are string)
+
+ 
+
---
+++ js/script2.js
@@ -0,0 +1,16 @@
+// Lets create a function which can add two numbers
+
+//1. Function defination (one time process) 
+
+function twoNumberAdd(n1,n2){  // 1. camalCase 2. function relevant name
+    // function body 
+    // every function return something
+    return n1 + n2 ;
+}
+// we can not create a varibal for keywords name 
+//2. Function calling/invoking  (many time process)
+
+// = Right to left 
+
+var result =  twoNumberAdd(100,200); // actual argument 
+console.log(result);
